f54b934eae323f71f69d21b9e3ee87a0a5c57ed7
Scarpaz14 <scarparofrancesco14@gmail.com>
2022-06-16 16:53:53 +0200

    aggiunto hero footer

---
+++ src/components/blocks/FooterComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <footer>
+    <Footerhero/>
+
+  </footer>
+</template>
+
+<script>
+import Footerhero from '../sections/FooterHero'
+ 
+export default {
+    name: 'FooterComponent',
+    components:{
+        Footerhero
+    }
+    
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/sections/FooterHero.vue
@@ -0,0 +1,26 @@
+<template>
+    <section id="last-section">
+        <div class="container">
+            <div class="footer-image">
+                <img class="img" src="../../assets/image/home-6-services-image.png" alt="">
+            </div>
+        </div>
+    </section>
+</template>
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+#last-section{
+    background: linear-gradient(white, #98989e);
+}
+
+.img{
+    width: 100%;
+    object-fit: cover;
+    background-position: center;
+}
+</style>
